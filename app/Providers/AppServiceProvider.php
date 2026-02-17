ebde0bda17009095e63e0ff83075f065c3b190e5
rayatech-dev <rayatechpangandaran@gmail.com>
2026-02-17 12:41:07 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View; 
 use App\Models\Faq;
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,16 +22,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('local')) {
+       if (!app()->environment('local')) {
         URL::forceScheme('https');
-
     }
-      $faqs = Faq::where('is_active', 1)
-                    ->orderBy('order')
-                    ->get();
 
-        // Share ke semua view
-        View::share('faqs', $faqs);
+    if (Schema::hasTable('faqs')) {
+        try {
+            $faqs = Faq::where('is_active', 1)
+                        ->orderBy('order')
+                        ->get();
+
+            View::share('faqs', $faqs);
+        } catch (\Exception $e) {
+            // biarin aja kalau gagal waktu build
+        }
+    }
 
     }
 }
